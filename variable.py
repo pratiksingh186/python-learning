556b9792e8a399da3652322faa10fe38376a42d7
pratiksingh186 <er.pratik01082006@gmail.com>
2026-05-26 01:18:35 +0530

    basic variable code of python

---
+++ variable.py
@@ -0,0 +1,49 @@
+# VARIABLE IN PYTHON
+
+#STRING VARIABLE
+name = "pratik singh "
+
+# integer variable 
+age = 20 
+
+# float variable 
+
+height = 183.5
+
+
+
+
+print(name)
+print(age)
+print(height)
+
+
+
+# checking data type 
+
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# basic maths operation 
+
+x = 66
+y = 16
+z = 18
+
+print(x*y*z)
+print(x+y+z)
+print(x/y)
+
+
+
+# user input 
+
+user_name = input("enter your name")
+
+print("hello ", user_name)
+
+
+
+
