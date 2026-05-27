af0dce41621cae40bb7094fb14bce84d541eba37
pratiksingh186 <er.pratik01082006@gmail.com>
2026-05-27 21:08:56 +0530

    data type in python to use in code

---
+++ data_type.py
@@ -0,0 +1,77 @@
+# DATA TYPE IN PYTHON 
+
+# INTEGER
+age = 20
+
+# float 
+
+height = 183.5
+
+#string
+
+name = "pratik singh (ps)"
+
+# complex number 
+complex_num = 3 + 4j
+
+# printing 
+
+print(age)
+print(height)
+print(name)
+print(complex_num)
+
+#checking data type 
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(complex_num))
+
+
+# LIST
+
+fruits = ["apple", "banana", "mango"]
+
+print(fruits)
+print(type(fruits))
+
+
+# TUPLE
+
+colors = ("red", "green", "blue")
+
+print(colors)
+print(type(colors))
+
+
+# DICTIONARY
+
+student = {
+    "name": "Rahul",
+    "age": 20,
+    "course": "Python"
+}
+
+print(student)
+print(type(student))
+
+
+# SET
+
+numbers = {1, 2, 3, 4, 5}
+
+print(numbers)
+print(type(numbers))
+
+
+
+# PRACTICE QUESTIONS
+
+# 1. Create variables of all basic data types
+# 2. Convert string "50" into integer
+# 3. Create a list of 5 fruits
+# 4. Create a dictionary for a student
+# 5. Print data type of each variable 
+
+
